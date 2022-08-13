3a5d2c30b11dfa7d1e26637807ed44185dc3acda
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2022-08-13 15:46:33 +0300

    initial commit

---
+++ src/ApiAuth/ApiAuth.Api/Options/SwaggerOptions.cs
@@ -0,0 +1,10 @@
+namespace ApiAuth.Api.Options;
+
+public class SwaggerOptions
+{
+    public string? JsonRoute { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? UiEndpoint { get; set; }
+}
---
+++ src/ApiAuth/ApiAuth.Api/Startup.cs
@@ -0,0 +1,47 @@
+using ApiAuth.Api.Options;
+using Microsoft.OpenApi.Models;
+
+namespace ApiAuth.Api;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    private IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "ApiAuth.Api", Version = "v1"}); });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(options => { options.RouteTemplate = swaggerOptions.JsonRoute; });
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
